177ae54f02b9232b1d541316322ad68e7fc76548
bryantb2 <bryantb2@my.lanecc.edu>
2019-10-14 11:25:30 -0700

    addded the OG version of the working lab1; added lab3 with new solution and passing strings to views via controllers

---
+++ Lab3/CommunitySite2ndEdition/CommunitySite2ndEdition/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCServeciesApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        public String Index()
+
+        {
+
+            /* Return a literal string instead of calling view() */
+
+            return "Never call me jackass!";
+
+        }
+    }
+}
---
+++ Lab3/CommunitySite2ndEdition/CommunitySite2ndEdition/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCServeciesApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        /* Change return type to String */
+
+        public IActionResult Index()
+
+        {
+
+            /* Return a literal string instead of calling view() */
+
+            return View("Index","Hello there");
+
+        }
+    }
+}
