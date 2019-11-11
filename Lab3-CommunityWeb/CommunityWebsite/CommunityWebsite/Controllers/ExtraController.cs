99398b002e96b6b078f0c595cb2636ab5351e254
Blake <bryantb2@my.lanecc.edu>
2019-11-11 12:30:07 -0800

    added extra custom return type methods for lab 6

---
+++ Lab3-CommunityWeb/CommunityWebsite/CommunityWebsite/Controllers/ExtraController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CommunityWebsite.Models;
+
+namespace CommunityWebsite.Controllers
+{
+    public class ExtraController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.BackgroundStyle = "pageContainer";
+            return View();
+        }
+
+        public String Okay()
+        {
+            return ("okay, you found me!");
+        }
+
+        public UnauthorizedResult BadSignin()
+        {
+            return Unauthorized();
+        }
+
+        public AcceptedResult GoodResult()
+        {
+            return Accepted();
+        }
+
+        public JsonResult ReturnUsers()
+        {
+            User newUser = new User("bob");
+            newUser.Password = "nice-Try-FBI!";
+            UserList.AddNewUser(newUser);
+            var jsonObject = UserList.ListOfUsers;
+
+            return Json(jsonObject);
+        }
+    }
+}
