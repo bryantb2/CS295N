194d7461500b25918e1668d20182005137c59fb4
Blake <bryantb2@my.lanecc.edu>
2019-11-11 16:05:17 -0800

    built static class that simulates an actual database ID assignment system for objects

--- TermProject/VSProject/BlogEngineProject/BlogEngineProject/Models/Comment.cs
+++ TermProject/VSProject/BlogEngineProject/BlogEngineProject/Models/Comment.cs
@@ -7,5 +7,9 @@ namespace BlogEngineProject.Models
 {
     public class Comment
     {
+        public int CommentInt { get; set; }
+        public String Username { get; set; }
+        public String CommentContent { get; set; }
+        public DateTime DatePublished { get; set; }
     }
 }
---
+++ TermProject/VSProject/BlogEngineProject/BlogEngineProject/ObjectIDBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogEngineProject
+{
+    public static class ObjectIDBuilder
+    {
+        // SUMMARY: this class will be used to grab a unique ID that can then be assigned to comments, posts, and threads,
+
+        // CLASS FIELDS
+        private static int CommentID = 0;
+        private static int PostID = 0;
+        private static int UserID = 0;
+        private static int ThreadID = 0;
+
+        public static int GetPostID()
+        {
+            int ID = PostID;
+            PostID += 1;
+            return ID;
+        }
+
+        public static int GetCommentID()
+        {
+            int ID = CommentID;
+            CommentID += 1;
+            return ID;
+        }
+
+        public static int GetUserID()
+        {
+            int ID = UserID;
+            UserID += 1;
+            return ID;
+        }
+
+        public static int GetThreadID()
+        {
+            int ID = ThreadID;
+            ThreadID += 1;
+            return ID;
+        }
+    }
+}
