6521a7d3404c76e2ee3d88238bd7df821deed8bc
Blake <bryantb2@my.lanecc.edu>
2019-11-11 16:31:36 -0800

    added Thread class model; fixed UML diagram to reflect changes in the Post model

--- TermProject/VSProject/BlogEngineProject/BlogEngineProject/Models/Thread.cs
+++ TermProject/VSProject/BlogEngineProject/BlogEngineProject/Models/Thread.cs
@@ -7,5 +7,31 @@ namespace BlogEngineProject.Models
 {
     public class Thread
     {
+        // auto implemented properties
+        public int ThreadID { get; set; }
+        public String Name { get; set; }
+        public String Bio { get; set; }
+        public String Category { get; set; }
+        public List<Post> Posts { get; set; }
+        public String ProfilePicURL { get; set; }
+
+        // METHODS
+        public void AddPostToThread(Post post) => Posts.Add(post);
+
+        public Post RemoveCommentFromHistory(int postID)
+        {
+            // find comment
+            // then remove it
+            Post removedPost = null;
+            foreach (Post p in Posts)
+            {
+                if (p.PostID == postID)
+                {
+                    removedPost = p;
+                    Posts.Remove(p);
+                }
+            }
+            return removedPost;
+        }
     }
 }
